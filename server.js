4e88c7fb537ff03d9523dbd0145fa4d233d8d4ac
Björn Tirsén <bjorntirsen@gmail.com>
2021-09-29 14:46:25 +0200

    :fire: Remove some console statements

--- server.js
+++ server.js
@@ -9,14 +9,7 @@ const database = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
-mongoose
-  .connect(database)
-  .then(() => {
-    console.log('DB connection successfull!');
-  })
-  .catch((error) => console.error(error));
+mongoose.connect(database).catch((error) => console.error(error));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+app.listen(port);
