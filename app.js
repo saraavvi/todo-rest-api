00547fc462ed2514f20ecdf3896262c0b2ab083c
Björn Tirsén <bjorntirsen@gmail.com>
2021-09-30 13:26:07 +0200

    :lock: Implement standard security features

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
 const express = require('express');
 const logger = require('morgan');
-// Implement CORS
+// Security resources
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const rateLimit = require('express-rate-limit');
+// CORS resources
 const cors = require('cors');
 const csp = require('express-csp');
 const cspConfig = require('./utils/cspConfig');
 
 const app = express();
+const AppError = require('./utils/AppError');
+const errorHandler = require('./utils/errorHandler');
+const listRouter = require('./routes/listRoutes');
+
+// Implement Security features
+// Set security HTTP headers
+app.use(helmet());
+// Limit request rates
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour.',
+});
+app.use('*', limiter);
+// Data sanitazion against NoSQL data injection
+app.use(mongoSanitize());
+// Data sanitazion against XSS
+app.use(xss());
+// Prevent param pollution
+app.use(hpp());
 
 // Implement CORS
 app.use(cors());
 app.options('*', cors());
 csp.extend(app, cspConfig);
-
-const listRouter = require('./routes/listRoutes');
-const AppError = require('./utils/AppError');
-const errorHandler = require('./utils/errorHandler');
 
 //Middleware
 app.use(logger('dev'));
