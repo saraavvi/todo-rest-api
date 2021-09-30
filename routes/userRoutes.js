2b5fbe3f034afb59a9706aca809eb2ddf7d6bc5d
Björn Tirsén <bjorntirsen@gmail.com>
2021-09-30 17:00:25 +0200

    :passport_control: Implement logout route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -71,4 +71,12 @@ router.post(
   })
 );
 
+router.get('/logout', (req, res) => {
+  res.cookie('jwt', 'loggedout', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({ status: 'success' });
+});
+
 module.exports = router;
