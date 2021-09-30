6988daed0a4c9379bd64ddc117114490ae7db92c
Björn Tirsén <bjorntirsen@gmail.com>
2021-09-30 13:57:44 +0200

    :bug: Fix name of function

--- controllers/listController.js
+++ controllers/listController.js
@@ -2,7 +2,7 @@ const List = require('../models/listModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/AppError');
 
-exports.createNewList = catchAsync(async (req, res, next) => {
+exports.createList = catchAsync(async (req, res, next) => {
   const newList = await List.create(req.body);
   res.status(201).json({
     status: 'success',
