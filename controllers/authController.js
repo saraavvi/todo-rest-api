701c6e70f0f2214c3fc1119e6cf8faa38510fa23
Björn Tirsén <bjorntirsen@gmail.com>
2021-10-01 13:56:07 +0200

    :sparkles: Add protect and getMe handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,8 @@
 const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
 
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
-
 const AppError = require('../utils/AppError');
 
 const signToken = (id) =>
@@ -70,3 +70,29 @@ exports.logout = (req, res) => {
   });
   res.status(200).json({ status: 'success' });
 };
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) Check if token exists
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  if (!token) {
+    return next(new AppError('You are not logged in. Log in to access.', 401));
+  }
+  // 2) Validate token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  // 3) Check if user still exists
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new AppError('The user belonging to the token does no longer exist', 401)
+    );
+  }
+  // Grant access
+  req.user = currentUser;
+  next();
+});
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const authController = require('../controllers/authController');
+const userController = require('../controllers/userController');
 
 const router = express.Router();
 
 router.post('/signup', authController.signup);
-
 router.post('/login', authController.login);
-
 router.get('/logout', authController.logout);
+
+router.get('/getMe', authController.protect, userController.getMe);
 
 module.exports = router;
