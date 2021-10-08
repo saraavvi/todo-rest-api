64bc36d35a9e79a51ca65da0eada414e773d6cdb
Sara Viktorsson <saraviktorsson@hotmail.com>
2021-10-08 08:55:47 +0200

    :white_check_mark: add test for create and delete list routes

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const app = require('../server');
 
 let token;
+let listId;
 const userCredentials = {
   email: process.env.TEST_USER_EMAIL,
   password: process.env.TEST_USER_PASSWORD,
@@ -25,6 +26,29 @@ describe('routes', () => {
         .get('/api/lists')
         .set({ Authorization: `Bearer ${token}` })
         .expect(200, done);
+    });
+  });
+
+  describe('POST api/lists', () => {
+    it('should respond with 201', (done) => {
+      request(app)
+        .post('/api/lists')
+        .send({ title: 'A title', body: 'some content' })
+        .set({ Authorization: `Bearer ${token}` })
+        .expect(201)
+        .end((err, res) => {
+          listId = res.body.data.list._id;
+          done();
+        });
+    });
+  });
+
+  describe(`DELETE api/lists/${listId}`, () => {
+    it('should respond with 204', (done) => {
+      request(app)
+        .delete(`/api/lists/${listId}`)
+        .set({ Authorization: `Bearer ${token}` })
+        .expect(204, done);
     });
   });
 });
