37087facfe0d74208893d6640027002629fe7190
Björn Tirsén <bjorntirsen@gmail.com>
2021-10-09 18:33:38 +0200

    :sparkles: Add  more description to tests

--- test/test.js
+++ test/test.js
@@ -8,38 +8,41 @@ const userCredentials = {
   password: process.env.TEST_USER_PASSWORD,
 };
 
-describe('routes', () => {
-  before((done) => {
-    request(app)
-      .post('/api/users/login')
-      .send(userCredentials)
-      .expect(200)
-      .end((err, res) => {
-        token = res.body.token;
-        done();
-      });
-  });
-
-  describe('GET api/lists', () => {
-    it('should respond with 200', (done) => {
+describe('Todo REST API routes', () => {
+  describe('Sending a POST request with correct username and password to /api/users/login', () => {
+    it('should respond with 200 and return json.', (done) => {
       request(app)
-        .get('/api/lists')
-        .set({ Authorization: `Bearer ${token}` })
+        .post('/api/users/login')
+        .send(userCredentials)
+        .expect((res) => {
+          token = res.body.token;
+        })
+        .expect('Content-Type', /json/)
         .expect(200, done);
     });
   });
 
-  describe('POST api/lists', () => {
-    it('should respond with 201', (done) => {
+  describe('Sending a GET request with a valid jwt to /api/lists', () => {
+    it('should respond with 200 and return json.', (done) => {
+      request(app)
+        .get('/api/lists')
+        .set({ Authorization: `Bearer ${token}` })
+        .expect('Content-Type', /json/)
+        .expect(200, done);
+    });
+  });
+
+  describe('Sending a POST request with a valid jwt and title and body to /api/lists', () => {
+    it('should respond with 201 and return json.', (done) => {
       request(app)
         .post('/api/lists')
         .send({ title: 'A title', body: 'some content' })
         .set({ Authorization: `Bearer ${token}` })
-        .expect(201)
-        .end((err, res) => {
+        .expect((res) => {
           listId = res.body.data.list._id;
-          done();
-        });
+        })
+        .expect('Content-Type', /json/)
+        .expect(201, done);
     });
   });
 
